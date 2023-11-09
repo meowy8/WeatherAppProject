044141a491bf2d9b815635bac9642c6c6f47963a
meowy8 <mhoey8@hotmail.com>
2023-11-09 16:33:50 +0000

    added weather icons and weather info

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ const visibilityValue = document.getElementById('visibility-value');
 const visibilityInfo = document.getElementById('visibility-info');
 const humidityValue = document.getElementById('humidity-value');
 const humidityInfo = document.getElementById('humidity-info');
+const dewPoint = document.getElementById('dew-point');
 const sunsetTime = document.getElementById('sunset-time');
 const sunriseTime = document.getElementById('sunrise-time');
 const airPollutionValue = document.getElementById('air-pollution-value');
@@ -43,6 +44,26 @@ const formattedTime = currentDate.toLocaleTimeString(undefined, timeOptions)
 date.innerText = formattedDate
 time.innerText = formattedTime
 
+const weatherIcons = {
+  1000: 'images/2682848_day_forecast_sun_sunny_weather_icon.png',
+  1003: 'images/2682849_cloud_cloudy_day_forecast_sun_icon.png',
+  1006: 'images/2682850_cloud_clouds_cloudy_forecast_weather_icon.png',
+  1009: 'images/2682850_cloud_clouds_cloudy_forecast_weather_icon.png',
+  1030: 'images/2682821_fog_foggy_forecast_mist_weather_icon.png',
+  1063: 'images/2682845_cloud_cloudy_forecast_rain_sun_icon.png',
+  1087: 'images/2682827_cloud_day_light bolt_rain_sun_icon.png',
+  1135: 'images/2682821_fog_foggy_forecast_mist_weather_icon.png',
+  1153: 'images/2682845_cloud_cloudy_forecast_rain_sun_icon.png',
+  1180: 'images/2682845_cloud_cloudy_forecast_rain_sun_icon.png',
+  1183: 'images/2682845_cloud_cloudy_forecast_rain_sun_icon.png',
+  1189: 'images/2682845_cloud_cloudy_forecast_rain_sun_icon.png',
+  1195: 'images/2682845_cloud_cloudy_forecast_rain_sun_icon.png',
+  1213: 'images/2682823_forecast_snow_snowflake_weather_icon.png',
+  1219: 'images/2682823_forecast_snow_snowflake_weather_icon.png',
+  1225: 'images/2682823_forecast_snow_snowflake_weather_icon.png',
+  1255: 'images/2682823_forecast_snow_snowflake_weather_icon.png',
+}
+
 const nowView = () => {
   nowButtonText.style.textDecoration = 'underline'
   todayButtonText.style.textDecoration = 'none'
@@ -55,6 +76,49 @@ const todayView = () => {
   nowButtonText.style.textDecoration = 'none'
   todayCardContainer.style.display = 'flex'
   nowCardContainer.style.display = 'none'
+}
+
+const uvIndexLevel = (uvIndex) => {
+  if (uvIndex <= 2) {
+    return 'Low'
+  } else if (3 <= uvIndex <= 5) {
+    return 'Moderate'
+  } else if (6 <= uvIndex <= 7) {
+    return 'High'
+  } else if (8 <= uvIndex <= 10) {
+    return 'Very High'
+  } else {
+    return 'Extreme'
+  }
+}
+
+const visibilityLevel = (visibility) => {
+  if (visibility < 1) {
+    return 'Very poor visibility'
+  } else if (1 <= visibility <= 4) {
+    return 'Poor visibility'
+  } else if (5 <= visibility <= 7) {
+    return 'Moderate visibility'
+  } else if (8 <= visibility <= 10) {
+    return 'Good visibility'
+  } else {
+    return 'Perfectly clear view'
+  }
+}
+
+const dewPointCalc = (temp, humidity) => {
+  const dewPointValue = temp - (100 - humidity)/5
+  return dewPointValue
+}
+
+const airPollutionLevel = (airPollution) => {
+  if (airPollution <= 50) {
+    return 'Good'
+  } else if (50 < airPollution <= 100) {
+    return 'Moderate'
+  } else {
+    return 'Unhealthy'
+  }
 }
 
 const todayWeatherFetch = (placeName) => {
@@ -82,7 +146,16 @@ const todayWeatherFetch = (placeName) => {
       overviewText.innerText = data.forecast.forecastday[0].day.condition.text
       chanceOfRain.innerHTML = data.forecast.forecastday[0].day.daily_chance_of_rain + '<span>% chance of rain</span>'
       avgTemp.innerText = data.forecast.forecastday[0].day.avgtemp_c + '°C'
-      highLowTemp.innerText = 'High: ' + data.forecast.forecastday[0].day.maxtemp_c + '\nLow: ' + data.forecast.forecastday[0].day.mintemp_c
+      highLowTemp.innerText = 'High: ' + data.forecast.forecastday[0].day.maxtemp_c + '°C' + '\nLow: ' + data.forecast.forecastday[0].day.mintemp_c + '°C'
+
+      const weatherIconCode = data.current.condition.code
+      weatherIcon.src = weatherIcons[weatherIconCode]
+
+      dewPoint.innerText = dewPointCalc(data.current.temp_c, data.current.humidity).toFixed(1)
+      uvInfo.innerText = uvIndexLevel(data.current.uv)
+      visibilityInfo.innerText = visibilityLevel(data.current.vis_miles)
+      precipitationInfo.innerHTML = data.forecast.forecastday[0].day.totalprecip_mm + '<span>mm of rain expected today</span>'
+      airPollutionInfo.innerText = airPollutionLevel(data.current.air_quality)
 
       detailedContainer.classList.remove('hidden')
       nowView()
